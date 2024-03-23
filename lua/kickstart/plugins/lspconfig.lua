13fea31967656280f1786f6035ae058b63857e70
marktlinn <marktlinn@gmail.com>
2024-03-23 22:22:36 +0100

    add: volar to lsp setup

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -7,9 +7,9 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+        -- Useful status updates for LSP.
+        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+        { 'j-hui/fidget.nvim', opts = {} },
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
@@ -178,24 +178,18 @@ return {
         goimports = {},
         gofumpt = {},
         golines = {},
+        -- js
+        tsserver = {},
+        volar = {},
+        --
         html = {},
         helm_ls = {},
         jsonls = {},
         sqlls = {},
-        tsserver = {},
         marksman = {},
         yamlls = {},
-        vuels = {},
         checkmake = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
         lua_ls = {
           -- cmd = {...},
           -- filetypes { ...},
